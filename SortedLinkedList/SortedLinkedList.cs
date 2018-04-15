44a6a07a74e2c0300045304f5f90aa345af76ed5
jalpendesai <djalpen24@gmail.com>
2018-04-14 23:14:11 -0400

    Enhanced Text Display

--- SortedLinkedList/SortedLinkedList.cs
+++ SortedLinkedList/SortedLinkedList.cs
@@ -9,24 +9,30 @@ namespace LinkedListLibrary
     {
         static void Main(string[] args)
         {
+            //List1
             List list = new List();
-
 
             list.InsertAtFront("10");
             list.InsertAtFront("50");
             list.InsertAtFront("30");
+            list.InsertAtBack("45");
             list.InsertAtFront("80");
             list.InsertAtFront("20");
+            list.InsertAtBack("82");
 
+            //List2
             List list2 = new List();
 
             list2.InsertAtFront("52");
             list2.InsertAtFront("58");
             list2.InsertAtFront("38");
+            list2.InsertAtBack("88");
             list2.InsertAtFront("85");
             list2.InsertAtFront("21");
 
             //Displaying Unsorted List
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\nUnsorted Lists: ");
             Console.Write("List1: ");
             list.Display();
@@ -34,6 +40,8 @@ namespace LinkedListLibrary
             list2.Display();
 
             //Sorting List
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\n\nSorted Lists:");
             List SortedList1 = list.Sort();
             List SortedList2 = list2.Sort();
@@ -43,6 +51,8 @@ namespace LinkedListLibrary
             list2.Display();
 
             //Merging list
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\n\nMerged List:");
             List mergedList = SortedList1.Merge(SortedList2);
             mergedList.Display();
